c59e5127f7e507bdc7483885b04132bead595a39
Riku Rouvila <riku.rouvila@gmail.com>
2015-02-13 01:21:05 +0200

    add request logger

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var express = require('express');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+morgan.token('body', function(req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 
 require('./routes/invite')(app);
 require('./routes/members')(app);
