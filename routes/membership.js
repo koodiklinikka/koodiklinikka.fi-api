c92a81376bdcdfccc3b38da6d0c4b7fde94cd269
Cihan Bebek <cihan.bebek@checkout.fi>
2017-07-05 14:32:11 +0300

    remove duplicate function

--- routes/membership.js
+++ routes/membership.js
@@ -34,29 +34,4 @@ module.exports = function (app) {
       }
     });
   });
-
-  app.post('/membership', function(req, res, next) {
-    stripe.charges.create({
-        amount: 1000,
-        currency: 'eur',
-        card: req.body.stripeToken,
-        description: `Koodiklinikka jäsenyys henkilölle: ${req.body.name}`
-    }, function(err, charge) {
-      if (err) {
-          console.log(JSON.stringify(err, null, 2));
-
-          var message = 'Membership payment FAILED for: ```' + JSON.stringify(req.body) + '``` Reason: ```' + err + '```';
-          console.log(message);
-          slack.createMessage(message);
-
-          res.status(500).send({message: 'payment_error'});
-          return;
-      } else {
-        var message = 'Membership payment SUCCESS for: ```' + JSON.stringify(req.body) + '```';
-        console.log(message);
-        slack.createMessage(message);
-        res.status(200).send({message: 'payment_success'});
-      }
-    });
-  });
 };
